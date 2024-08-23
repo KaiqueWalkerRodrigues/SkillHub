7556fb9efb4a033222c575ab70693d920c6dec32
KaiqueWalkerRodrigues <108485267+KaiqueWalkerRodrigues@users.noreply.github.com>
2024-08-23 13:04:44 -0300

    navbar

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SkillHub - Ínicio</title>
+
+    <?php include_once('link.php') ?>
 </head>
 <body>
 
