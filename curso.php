f148d144e5159b9b40a81e765bf1df02d228029f
KaiqueWalkerRodrigues <108485267+KaiqueWalkerRodrigues@users.noreply.github.com>
2024-09-04 21:38:00 -0300

    aula

--- curso.php
+++ curso.php
@@ -2,6 +2,7 @@
     include_once('class/classes.php');
 
     $Cursos = new Curso();
+    $Aulas = new Aula();
 
     $curso = $Cursos->mostrar($_GET['id']);
 ?>
@@ -30,44 +31,26 @@
             <div class="row">
                 <div class="row">
 
-                    <div class="row mb-4" id="aula1">
+                    <?php
+                        foreach($Aulas->listar() as $aula){
+                            $n_aula = 1;
+                    ?>
+                    <div class="row mb-4">
                         <div class="col-3">
                             <!-- Botão que abre o modal -->
-                            <a href="#" style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#videoModal">
-                                <h5 class="text-center">Aula 1</h5>
+                            <a href="#" style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#videoModal" data-link="<?php echo $aula->link ?>">
+                                <h5 class="text-center">Aula <?php echo $n_aula ?></h5>
                                 <div class="card curso p-3" style="width: 18rem; margin: 0 auto;">
-                                    <img src="img/miniatura1.png" class="card-img-top" alt="">
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-3">
-                            <!-- Botão que abre o modal -->
-                            <a href="#" style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#videoModal">
-                                <h5 class="text-center">Aula 2</h5>
-                                <div class="card curso p-3" style="width: 18rem; margin: 0 auto;">
-                                    <img src="img/miniatura1.png" class="card-img-top" alt="">
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-3">
-                            <!-- Botão que abre o modal -->
-                            <a href="#" style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#videoModal">
-                                <h5 class="text-center">Aula 3</h5>
-                                <div class="card curso p-3" style="width: 18rem; margin: 0 auto;">
-                                    <img src="img/miniatura1.png" class="card-img-top" alt="">
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-3">
-                            <!-- Botão que abre o modal -->
-                            <a href="#" style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#videoModal">
-                                <h5 class="text-center">Aula 4</h5>
-                                <div class="card curso p-3" style="width: 18rem; margin: 0 auto;">
-                                    <img src="img/miniatura1.png" class="card-img-top" alt="">
+                                    <img src="img/miniatura1.png" class="card-img-top">
                                 </div>
                             </a>
                         </div>
                     </div>
+                    <?php 
+                            $n_aula++;
+                        }
+                    ?>
+
                     <div class="col-6 offset-3 mt-3">
                         <a href="" style="text-decoration: none;" id="abrirQuiz" data-bs-toggle="modal" data-bs-target="#quizModal">
                             <h5 class="text-center">Quiz do Curso</h5>
@@ -83,21 +66,21 @@
     </div>
 
     <div class="modal fade" id="videoModal" tabindex="-1" aria-labelledby="videoModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-xl">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="videoModalLabel">Aula 1 - Introdução ao Uso de Computadores</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <!-- Player de vídeo local -->
-                    <div class="ratio ratio-16x9">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/oCNV0lXfXU0?si=1hSQUbZS2IacPGLi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                    </div>
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="videoModalLabel">Aula 1 - Introdução ao Uso de Computadores</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <!-- Player de vídeo local -->
+                <div class="ratio ratio-16x9">
+                    <iframe width="560" height="315" id="aulaVideo" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
     <div class="modal fade" id="quizModal" tabindex="-1" aria-labelledby="quizModalLabel" aria-hidden="true">
         <form action="?" method="post">
@@ -114,7 +97,7 @@
                                 <input type="text" name="questao1" id="questao1" class="form-control" required>
                             </div>
                             <div class="col-5 text-center">
-                                <label for="questao2" class="form-label">Questão 1</label>
+                                <label for="questao2" class="form-label">Questão 2</label>
                                 <select name="questao2" id="questao2" class="form-control" required>
                                     <option value="">Selecione...</option>
                                 </select>
@@ -130,8 +113,23 @@
         </form>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script>
+    $('#videoModal').on('show.bs.modal', function (event) {
+        let button = $(event.relatedTarget);
+        let link = button.data('link');
 
+        // Corrige o src do iframe
+        $('#aulaVideo').attr('src', link);
+    });
+
+    $('#videoModal').on('hide.bs.modal', function () {
+        // Remove o src do iframe ao fechar o modal para parar o vídeo
+        $('#aulaVideo').attr('src', '');
+    });
+</script>
+
+    
     <!-- Scripts do Bootstrap (necessário para o funcionamento do modal) -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz4fnFO9gyb7m00IVIyjDZHn2gFp51RoShbZp6NAM0CfaQrZy0pP4pEP8s" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuQ7g5p1QolTMZZ5RdYUV02pAqO95nMNylVvC82bg4l7o1EDvF4Np5dAJ9HgEyjE" crossorigin="anonymous"></script>
