0fbfa5f7de971810c2e218c29539133ce8459c5f
KaiqueWalkerRodrigues <108485267+KaiqueWalkerRodrigues@users.noreply.github.com>
2024-09-09 09:47:59 -0300

    banco de dados 09.09.2024

--- banco.sql
+++ banco.sql
@@ -65,7 +65,7 @@ CREATE TABLE `cursos` (
 
 LOCK TABLES `cursos` WRITE;
 /*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
-INSERT INTO `cursos` VALUES (1,'Introdução ao Uso de Computadores','https://s2-techtudo.glbimg.com/8rng1sV5afk_6WZRli1PKTvoy7w=/0x0:1500x1000/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2019/n/R/kdN4P7SNuOq8mq7CUo8Q/techtudo-banner.jpg','<p>Este <strong>curso introdutório</strong> é ideal para quem deseja aprender os conceitos básicos de informática. Ele abrange tópicos essenciais como:</p>\n<ul>\n  <li>Entendimento do <em>hardware</em> e <em>software</em> de um computador.</li>\n  <li>Uso de sistemas operacionais como Windows e Linux.</li>\n  <li>Noções de segurança digital.</li>\n  <li>Práticas de navegação na internet e uso de e-mails.</li>\n</ul>\n<p>Ao final do curso, o aluno estará apto a utilizar o computador com mais confiança e eficiência no dia a dia.</p>');
+INSERT INTO `cursos` VALUES (1,'Introdução ao Uso de Computadores','https://s2-techtudo.glbimg.com/8rng1sV5afk_6WZRli1PKTvoy7w=/0x0:1500x1000/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2019/n/R/kdN4P7SNuOq8mq7CUo8Q/techtudo-banner.jpg','<p>Apresentar aos alunos uma visão geral sobre o que é um computador, como ele funciona e seus principais componentes.</p>');
 /*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,11 +80,10 @@ CREATE TABLE `perguntas` (
   `id_pergunta` int NOT NULL AUTO_INCREMENT,
   `id_quiz` int NOT NULL,
   `pergunta` text NOT NULL,
-  `resposta_certa` text NOT NULL,
   PRIMARY KEY (`id_pergunta`),
   KEY `fk_perguntas_quizzes_idx` (`id_quiz`),
   CONSTRAINT `fk_perguntas_quizzes` FOREIGN KEY (`id_quiz`) REFERENCES `quizzes` (`id_quiz`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,6 +92,7 @@ CREATE TABLE `perguntas` (
 
 LOCK TABLES `perguntas` WRITE;
 /*!40000 ALTER TABLE `perguntas` DISABLE KEYS */;
+INSERT INTO `perguntas` VALUES (1,1,'Qual é a função principal da CPU em um computador?'),(2,1,'Qual componente do computador é utilizado para inserir dados através de letras e números?'),(3,1,'Qual é a sequência correta para desligar um computador?'),(4,1,'Qual dos seguintes cuidados NÃO é necessário para a manutenção básica de um computador?'),(5,1,'O que é necessário fazer antes de ligar o computador?');
 /*!40000 ALTER TABLE `perguntas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -109,7 +109,7 @@ CREATE TABLE `quizzes` (
   PRIMARY KEY (`id_quiz`),
   KEY `fk_quizzes_cursos_idx` (`id_curso`),
   CONSTRAINT `fk_quizzes_cursos` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id_curso`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -118,6 +118,7 @@ CREATE TABLE `quizzes` (
 
 LOCK TABLES `quizzes` WRITE;
 /*!40000 ALTER TABLE `quizzes` DISABLE KEYS */;
+INSERT INTO `quizzes` VALUES (1,1);
 /*!40000 ALTER TABLE `quizzes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -132,10 +133,11 @@ CREATE TABLE `respostas` (
   `id_resposta` int NOT NULL AUTO_INCREMENT,
   `id_pergunta` int NOT NULL,
   `resposta` text NOT NULL,
+  `correta` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_resposta`),
   KEY `fk_respostas_perguntas_idx` (`id_pergunta`),
   CONSTRAINT `fk_respostas_perguntas` FOREIGN KEY (`id_pergunta`) REFERENCES `perguntas` (`id_pergunta`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -144,6 +146,7 @@ CREATE TABLE `respostas` (
 
 LOCK TABLES `respostas` WRITE;
 /*!40000 ALTER TABLE `respostas` DISABLE KEYS */;
+INSERT INTO `respostas` VALUES (1,1,'Exibir imagens e vídeos.',0),(2,1,'Processar informações e executar instruções.',1),(3,1,'Armazenar arquivos e documentos.',0),(4,1,'Conectar o computador à internet.',0),(5,2,'Monitor',0),(6,2,'CPU',0),(7,2,'Teclado',1),(8,2,'Mouse',0),(9,3,'Fechar todos os programas e desconectar da energia.',0),(10,3,'Pressionar o botão de energia até o computador desligar.',0),(11,3,'Acessar o menu \'Iniciar\' e selecionar \'Desligar\'.',1),(12,3,'Desligar o estabilizador e depois o computador.',0),(13,4,'Limpar o monitor, teclado e mouse regularmente.',0),(14,4,'Desligar o computador corretamente pelo menu \'Iniciar\'.',0),(15,4,'Manter o computador sempre ligado para evitar falhas.',1),(16,4,'Usar protetores contra surtos elétricos.',0),(17,5,'Ligar o monitor primeiro.',0),(18,5,'Verificar se todas as conexões estão corretas e ligar o estabilizador ou nobreak.',1),(19,5,'Apertar o botão do mouse.',0),(20,5,'Desconectar todos os cabos para evitar sobrecarga.',0);
 /*!40000 ALTER TABLE `respostas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -161,7 +164,7 @@ CREATE TABLE `usuarios` (
   `nome` varchar(150) NOT NULL,
   `cpf` char(11) NOT NULL,
   PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -170,6 +173,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'kaique.souza','SkggAh44MvTP6','Kaique Rodrigues','37498968878');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -211,4 +215,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-04 21:38:32
+-- Dump completed on 2024-09-09  9:47:51
